83e44a6d2e33cfa49907e0238794984f7cbd1ae1
Ritonun <jeremy.moreau2001@gmail.com>
2022-04-30 18:34:49 +0200

    Settings take config value from settings.ini

---
+++ src/settings.py
@@ -0,0 +1,21 @@
+''' --- SETTINGS --- '''
+from config import config
+
+
+def get_int_value(section, key):
+    value = config[section][key]
+    value = int(value)
+    return value
+
+
+# Display
+WIDTH = get_int_value("display", "width")
+HEIGHT = get_int_value("display", "height")
+
+# Fractal value
+# Plot window (RE = x_axis, IM = y__axis)
+# Default value: RE_S=-2, RE_E=1, IM_S=-1, IM_E=1
+RE_START = get_int_value("fractal", "re_start")
+RE_END = get_int_value("fractal", "re_end")
+IM_START = get_int_value("fractal", "im_start")
+IM_END = get_int_value("fractal", "im_end")
