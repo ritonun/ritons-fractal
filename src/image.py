f2570707807166dfee6ddb30aa57ca2848e8a045
Ritonun <jeremy.moreau2001@gmail.com>
2022-05-01 17:18:52 +0200

    fixed render_image

--- src/image.py
+++ src/image.py
@@ -4,7 +4,7 @@ from fractal_math import mandelbrot
 from utils import loading
 
 
-def render_image(save=False, show=True):
+def render_image(show_loading=True):
     img = Image.new('RGB', (WIDTH, HEIGHT), (0, 0, 0))
     draw = ImageDraw.Draw(img)
 
@@ -13,12 +13,11 @@ def render_image(save=False, show=True):
 
             c = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
                         IM_START + (y / HEIGHT) * (IM_END - IM_START))
-
             m = mandelbrot(c)
-
             color = 255 - int(m * 255 / MAX_ITER)
-
             draw.point([x, y], (color, color, color))
-        loading.show_loading_msg(x, WIDTH)
+
+        if show_loading:
+            loading.show_loading_msg(x, WIDTH)
 
     return img
