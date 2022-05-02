566570a7d0e0ec25737ea644d169fa086841e949
Ritonun <jeremy.moreau2001@gmail.com>
2022-05-02 14:56:09 +0200

    dixe documentation

--- src/animation.py
+++ src/animation.py
@@ -5,6 +5,8 @@ from utils import Loading
 
 def anim(iteration, func, new_re_start=None, new_re_end=None, 
          new_im_start=None, new_im_end=None):
+    ''' Take new re and im value, and create x image that progressively zoom 
+    in, so you can create a gif/video by combining all image '''
     loading = Loading()
     reload_settings()
     config = Config()
@@ -17,6 +19,7 @@ def anim(iteration, func, new_re_start=None, new_re_end=None,
         incrementation['im_start'] = (cs.IM_START - new_im_start) / iteration
     if new_im_end is not None:
         incrementation['im_end'] = (cs.IM_END - new_im_end) / iteration
+
     for i in range(0, iteration):
         if new_re_start is not None:
             config.modify_settings("fractal", "re_start", cs.RE_START - incrementation['re_start'])
