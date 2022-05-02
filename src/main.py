b20383ffb991a0a2f072c6b537bd0aaa24f30ad1
Ritonun <jeremy.moreau2001@gmail.com>
2022-05-02 14:37:51 +0200

    test

--- src/main.py
+++ src/main.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     time_math.start_running_time()
     config = Config()
     config.write_default_settings()
-    config.modify_settings("fractal", "max_iter", 200)
+    config.modify_settings("fractal", "max_iter", 80)
     config.modify_settings("display", "width", 600)
     config.modify_settings("display", "height", 400)
-    anim(2, main, new_re_start=-0.35, new_re_end=-0.08, new_im_start=-0.775, 
+    anim(5, main, new_re_start=-0.35, new_re_end=-0.08, new_im_start=-0.775, 
          new_im_end=-0.605)
     time_math.show_running_time()
